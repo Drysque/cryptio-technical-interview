09b5e7047f57b5eb465419f18452921663f95761
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2021-02-02 23:47:28 +0100

    Added PORT from env

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import * as env from 'env-var';
 
 const PORT = env.get('PORT').asPortNumber() || 8080;
-const NODE_ENV = env.get('NODE_ENV').asString() ?? 'unknown environment';
+const NODE_ENV = env.get('NODE_ENV').asEnum(['development', 'production']) ?? 'unknown environment';
 
 const app = express();
 
@@ -14,6 +14,4 @@ app.get('/ping', (_, res) => {
   res.send('pong');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT} (${NODE_ENV})`);
-});
+app.listen(PORT, () => console.log(`Server listening on port ${PORT} (${NODE_ENV})`));
