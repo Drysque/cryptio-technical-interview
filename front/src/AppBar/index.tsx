1181c3f2f7be82deabb063c966cd19ef92bf2fe6
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2021-02-03 20:30:25 +0100

    Added AppBar

---
+++ front/src/AppBar/index.tsx
@@ -0,0 +1,17 @@
+import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import { GitHub } from '@material-ui/icons/';
+
+export default () => {
+  const openGithub = () => window.open('https://github.com/Drysque/cryptio-technical-interview');
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        Bitcoin Historical Balances
+        <IconButton edge="end" onClick={openGithub}>
+          <GitHub />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
