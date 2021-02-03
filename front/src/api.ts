146d639e25b0e10996ee716986e4fe9b8a808625
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2021-02-03 20:30:15 +0100

    Added some api functions

--- front/src/api.ts
+++ front/src/api.ts
@@ -1,3 +1,24 @@
+import axios from 'axios';
 import * as env from 'env-var';
 
-const API_ENDPOINT = env.get('API_ENDPOINT').asUrlString() || 'localhost:8080';
+const API_ENDPOINT = env.get('API_ENDPOINT').asUrlString() || 'http://localhost:8080';
+
+export const getHealth = (): Promise<void> =>
+  axios
+    .get(`${API_ENDPOINT}/health`)
+    .then(() => Promise.resolve())
+    .catch((err) => Promise.reject(err));
+
+export const getAddressInfo = (addr: string): void => {
+  axios
+    .get(`${API_ENDPOINT}/crypto/address`, { params: { a: addr } })
+    .then(console.log)
+    .catch(console.error);
+};
+
+export const getAddressTransaction = (addr: string, page: number): void => {
+  axios
+    .get(`${API_ENDPOINT}/crypto/address`, { params: { a: addr, p: page } })
+    .then(console.log)
+    .catch(console.error);
+};
